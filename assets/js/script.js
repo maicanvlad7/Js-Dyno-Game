661a636f701eedd1c5f1e62c0ded861a8f7177a3
maicanvlad7 <maicanvlad1998@gmail.com>
2020-07-29 14:36:00 +0300

    added functionalities to the game- design is bad

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,18 @@
 const character = document.getElementById('character');
 const obstacle = document.getElementById('obstacle');
 
+document.addEventListener('keydown', event => {
+    if(event.keyCode === 32) {
+        jump();
+    }
+})
+
 function jump(){
 
     if ( !character.classList.contains('animate-char') ) {
         character.classList.add('animate-char');
 
-        setTimeout(function(){
+        setTimeout( ()=> {
             character.classList.remove('animate-char');
         },1000)
     }
@@ -20,12 +26,16 @@ var gameState = 0;
 function start(){
     if (!gameState) {
         obstacle.classList.add('block');
+        gameState = 1;
     }
 }
 
 // stops game but doesn't reset score
 function stop(){
-
+    if (gameState) {
+        obstacle.classList.remove('block');
+        gameState = 0;
+    }
 }
 
 //stops game and resets score
